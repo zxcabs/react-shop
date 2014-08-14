fffc338772d8ae5e77c831c2b931c77936131bdd
Vsevolod Ivanov <www@sevolod.ru>
2014-08-15 03:04:23 +0400

    vagrant

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list
 apt-get update
-apt-get install mongodb-org nodejs npm
+apt-get install mongodb-org nodejs npm git
 ln -s /usr/bin/nodejs /usr/bin/node
 npm install -g nodemon forever
 npm install -g gulp
