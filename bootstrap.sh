b74d7d6d4efcf17111eb985bb98732880b3a5b00
Vsevolod Ivanov <www@sevolod.ru>
2014-08-15 12:10:12 +0400

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,11 +5,6 @@ echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | 
 apt-get update
 apt-get install mongodb-org nodejs npm git
 ln -s /usr/bin/nodejs /usr/bin/node
-npm install -g nodemon forever
-npm install -g gulp
+npm install -g nodemon forever gulp
 cd /vagrant
 sudo npm install
-
-TZ='Europe/Kiev'; export TZ
-echo "TZ='Europe/Kiev'; export TZ" >> ~/.profile
-echo "TZ='Europe/Kiev'; export TZ" >> ~vagrant/.profile
